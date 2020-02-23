1b5eca4c9a38c98b5718140eacf36f9726194cef
Mullice <5235063+mullice23@user.noreply.gitee.com>
2020-02-23 14:32:13 +0800

    最开始

---
+++ src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+    home
+</div>
+</template>
+
+<script>
+export default {
+  created () {
+
+  },
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang='less' scoped>
+
+</style>
